5b47f2d60c43339252539d44b073263cef01ff70
Adrian Hornung <adrian.hornung@yahoo.de>
2024-06-05 22:44:29 +0200

    pass  Execute a program

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,57 @@
 use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(clap::Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand, Debug, Clone)]
+#[command(version, about, long_about = None)]
+enum Command {
+    #[command(about = "the run command")]
+    Run(RunCommand),
+}
+
+#[derive(clap::Args, Debug, Clone)]
+struct RunCommand {
+    image: String,
+    command: String,
+    #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
+    args: Vec<String>,
+}
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage!
-    // let args: Vec<_> = std::env::args().collect();
-    // let command = &args[3];
-    // let command_args = &args[4..];
-    // let output = std::process::Command::new(command)
-    //     .args(command_args)
-    //     .output()
-    //     .with_context(|| {
-    //         format!(
-    //             "Tried to run '{}' with arguments {:?}",
-    //             command, command_args
-    //         )
-    //     })?;
-    //
-    // if output.status.success() {
-    //     let std_out = std::str::from_utf8(&output.stdout)?;
-    //     println!("{}", std_out);
-    // } else {
-    //     std::process::exit(1);
-    // }
+    match &args.command {
+        Command::Run(com) => run(com),
+    }
+}
+
+fn run(com: &RunCommand) -> Result<()> {
+    let output = std::process::Command::new(&com.command)
+        .args(&com.args)
+        .output()
+        .with_context(|| {
+            format!(
+                "Tried to run '{}' with arguments {:?}",
+                com.command, com.args
+            )
+        })?;
+
+    if output.status.success() {
+        let std_out = std::str::from_utf8(&output.stdout)?;
+        println!("{}", std_out);
+    } else {
+        std::process::exit(1);
+    }
 
     Ok(())
 }
